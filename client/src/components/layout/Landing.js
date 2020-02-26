a7c8bae4ce945d54543cc027b6bf906f211687b3
arnachap <arnaud.chapeleur@gmail.com>
2020-02-26 10:39:42 +0100

    Landing header buttons

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,30 +4,30 @@ import Navbar from '../layout/Navbar';
 import Particles from 'react-particles-js';
 
 const Landing = () => {
+  const particlesParams = {
+    particles: {
+      opacity: {
+        random: true,
+        anim: {
+          enable: false
+        }
+      },
+      size: {
+        value: 2
+      },
+      move: {
+        speed: 1
+      },
+      line_linked: {
+        enable: false
+      }
+    }
+  };
+
   return (
     <Fragment>
-      <Particles
-        style={{ position: 'absolute' }}
-        params={{
-          particles: {
-            opacity: {
-              random: true,
-              anim: {
-                enable: false
-              }
-            },
-            size: {
-              value: 2
-            },
-            move: {
-              speed: 1
-            },
-            line_linked: {
-              enable: false
-            }
-          }
-        }}
-      />
+      <Particles style={{ position: 'absolute' }} params={particlesParams} />
+
       <Navbar />
 
       <div id='landing-header'>
@@ -35,11 +35,20 @@ const Landing = () => {
           <div className='row align-items-center'>
             <div className='col-md-6'>
               <h1>Le réseau social privé</h1>
+
               <p>
                 Chez Tribu, notre mission est de vous fournir un réseau social
                 privé, ou seul vous et les membres de vos groupes peuvent
                 accéder à votre profil.
               </p>
+
+              <Link to='/login' className='btn btn-primary'>
+                Se connecter
+              </Link>
+
+              <Link to='/register' className='btn btn-secondary ml-2'>
+                S'inscrire
+              </Link>
             </div>
 
             <div className='col-md-6'>
